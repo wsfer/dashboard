3221d455ed3eb78d73fa71d7b787208590e71c69
wesley <wesley_snaine@hotmail.com>
2022-06-05 23:26:08 -0300

    create script to change dark/light theme

--- script.js
+++ script.js
@@ -1,2 +1,21 @@
 const root = document.documentElement;
-root.className = 'dark';
+root.className = 'light';
+
+const sun = document.querySelector('.sun');
+const moon = document.querySelector('.moon');
+moon.style.display = 'none';
+
+const theme = document.querySelector('.changeTheme');
+theme.addEventListener('click', changeTheme);
+
+function changeTheme () {
+    if (root.className === 'dark') {
+        root.className = 'light';
+        moon.style.display = 'none';
+        sun.style.display = 'block';
+    } else {
+        root.className = 'dark';
+        moon.style.display = 'block';
+        sun.style.display = 'none';
+    }
+}
